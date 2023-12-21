da2afe7b4110f2a309c48bb240111f3107b7e3cf
Bluspur <craig-home@hotmai.com>
2023-12-21 13:35:58 +0000

    initial commit

---
+++ part_2/src/main.rs
@@ -0,0 +1,24 @@
+use anyhow::Result;
+use rayon::prelude::*;
+
+fn main() {
+    let input = std::fs::read_to_string("./puzzle_input.txt").expect("Unable to read file");
+    let result = solve_part(&input);
+    println!("Result: {}", result);
+}
+
+fn solve_part(input: &str) -> usize {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+
+    #[test]
+    fn test_solve_part() {
+        let input = indoc! {"
+            000
+        "};
+        assert_eq!(solve_part(input), 0);
+    }
+}
